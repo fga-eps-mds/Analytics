04c28a2ae830948467c239699b59bc346adffc45
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2024-09-18 18:46:31 -0300

    Juncao parser github e sonar

--- parser_template.py
+++ parser_template.py
@@ -5,8 +5,27 @@ from datetime import datetime
 import requests
 # import datetime
 import pandas as pd
+import os
+from packaging import version
+from dotenv import load_dotenv
 
+######################################
+# DECLARAÇÃO DE CONSTANTES/VARIÁVEIS #
+######################################
 TODAY = datetime.now()
+
+load_dotenv()
+# Variáveis globais ao repositório
+OWNER = "fga-eps-mds"
+REPO = os.getenv('REPO')
+REPO_ISSUES = os.getenv('REPO_DOC')
+
+# Configurar as variáveis de ambiente
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+RELEASE_MAJOR = os.getenv('RELEASE_MAJOR')
+RELEASE_MINOR = os.getenv('RELEASE_MINOR')
+RELEASE_FIX = os.getenv('RELEASE_FIX')
+DEVELOP = os.getenv('DEVELOP')
 
 METRICS_SONAR = [
     "files",
@@ -23,74 +42,127 @@ METRICS_SONAR = [
     "security_rating",
 ]
 
-BASE_URL = "https://sonarcloud.io/api/measures/component_tree?component=fga-eps-mds_"
+BASE_URL_SONAR = "https://sonarcloud.io/api/measures/component_tree?component=fga-eps-mds_"
 
-if __name__ == "__main__":
+# Utilize a api que for necessária
+# api_url_workflows = f"https://api.github.com/repos/{owner}/{repo}/actions/workflows"
+# api_url_jobs = f"https://api.github.com/repos/{owner}/{repo}/actions/runs/3624383254/jobs"
+# api_url_deployments = f"https://api.github.com/repos/{owner}/{repo}/deployments"
+api_url_runs = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/runs"
+api_url_issues = f"https://api.github.com/repos/{OWNER}/{REPO_ISSUES}/issues"
 
-    REPO = sys.argv[1]
-    RELEASE_VERSION = sys.argv[2]
+###################
+# FUNÇÕES RELEASE #
+###################
+# Pega a última release
+def get_latest_release():
+    url = f'https://api.github.com/repos/{OWNER}/{REPO}/releases'
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}'
+    }
+    response = requests.get(url, headers=headers)
+    releases = response.json()
+    
+    if releases:
+        return releases[0].get('tag_name', '0.0.0')
+    return '0.0.0'
 
-    response = requests.get(
-        f'{BASE_URL}{REPO}&metricKeys={",".join(METRICS_SONAR)}&ps=500'
-    )
+# Cria um novo nome de tag
+def new_tag_name():
+    old_tag = get_latest_release()
+    try:
+        old_version = version.parse(old_tag)
+    except version.InvalidVersion:
+        old_version = version.parse('0.0.0')
+
+    if RELEASE_MAJOR == 'true':
+        return f'{old_version.major + 1}.0.0'
+    elif RELEASE_MINOR == 'true':
+        return f'{old_version.major}.{old_version.minor + 1}.0'
+    elif RELEASE_FIX == 'true':
+        return f'{old_version.major}.{old_version.minor}.{old_version.micro + 1}'
+    else:
+        return f'{old_version.major}.{old_version.minor}.{old_version.micro + 1}'
+    
+# Cria a nova release
+def create_release():
+    tag = new_tag_name()
+    url = f'https://api.github.com/repos/{OWNER}/{REPO}/releases'
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    payload = {
+        'tag_name': tag,
+        'name': tag
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    res_data = response.json()
+    return res_data.get('upload_url'), tag
+
+#################
+# FUNÇÕES SONAR #
+#################
+
+def save_sonar_metrics(tag):
+    response = requests.get(f'{BASE_URL_SONAR}{REPO}&metricKeys={",".join(METRICS_SONAR)}&ps=500')
+
     j = json.loads(response.text)
 
-    file_path = f'./analytics-raw-data/fga-eps-mds-{REPO}-{TODAY.strftime("%m-%d-%Y-%H-%M-%S")}-{RELEASE_VERSION}.json'
+    print("Extração do Sonar concluída.")
 
-    with open(file_path, "w") as fp:
+    file_path = f'./analytics-raw-data/fga-eps-mds-{REPO}-{TODAY.strftime("%m-%d-%Y-%H-%M-%S")}-{tag}.json'
+
+    with open(file_path, 'w') as fp:
         fp.write(json.dumps(j))
         fp.close()
-        
-# _______________________________________________________________________________________________________________________________
 
-    nome_arquivo_json = f'./analytics-raw-data/GitHub_API-fga-eps-mds-{REPO}-{TODAY.strftime("%m-%d-%Y-%H-%M-%S")}-{RELEASE_VERSION}.json'
+    return
 
-    owner = "fga-eps-mds"
-    repo = "2022-2-MeasureSoftGram-Core"
-    # date = datetime.datetime.now().strftime("%Y-%m-%d")
-    date = datetime.strptime("2023-03-23","%Y-%m-%d").strftime("%Y-%m-%d")
+##################
+# FUNÇÕES GITHUB #
+##################
 
-    # 2023-03-23T01:56:10Z
+def all_request_pages(data):
+    total_runs = data["total_count"]
+    pages = (total_runs // 100) + (1 if total_runs % 100 > 0 else 0)
+    for i in range(pages+1):
+        if i == 0 or i == 1:
+            continue
+        api_url_now = api_url_runs + "?page=" + str(i)
+        response = requests.get(api_url_now)
+        for j in ((response.json()['workflow_runs'])):
+            data['workflow_runs'].append(j)
+    return data
 
-    # Utilize a api que for necessária
-    # api_url_workflows = f"https://api.github.com/repos/{owner}/{repo}/actions/workflows"
-    # api_url_jobs = f"https://api.github.com/repos/{owner}/{repo}/actions/runs/3624383254/jobs"
-    # api_url_deployments = f"https://api.github.com/repos/{owner}/{repo}/deployments"
-    api_url_runs = f"https://api.github.com/repos/{owner}/{repo}/actions/runs"
+def filter_request_per_date(data, date):
+    data_filtered = []
+    for i in data["workflow_runs"]:
+        if datetime.strptime(i["created_at"][:10],"%Y-%m-%d").strftime("%Y-%m-%d") == date:
+            data_filtered.append(i)
+    return {"workflow_runs": data_filtered}
 
+def save_github_metrics_runs():
+    response = requests.get(api_url_runs, params={'per_page': 100,})
 
-    def all_request_pages(data):
-        total_runs = data["total_count"]
-        pages = (total_runs // 100) + (1 if total_runs % 100 > 0 else 0)
-        for i in range(pages+1):
-            if i == 0 or i == 1:
-                continue
-            api_url_now = api_url_runs + "?page=" + str(i)
-            response = requests.get(api_url_now)
-            for j in ((response.json()['workflow_runs'])):
-                data['workflow_runs'].append(j)
-        return data
-
-
-    def filter_request_per_date(data, date):
-        data_filtered = []
-        for i in data["workflow_runs"]:
-            if datetime.strptime(i["created_at"][:10],"%Y-%m-%d").strftime("%Y-%m-%d") == date:
-                data_filtered.append(i)
-        return {"workflow_runs": data_filtered}
-
-    # Envia get request
-    response = requests.get(api_url_runs,params={'per_page': 100,})
-
-    # get the data in json or equivalent dict form at
     data = response.json()
 
-    data =  filter_request_per_date(all_request_pages(data),date)
+    # date = datetime.strptime("2023-03-23","%Y-%m-%d").strftime("%Y-%m-%d")
+    data = all_request_pages(data)
 
     print("Quantidade de workflow_runs: " + str(len(data["workflow_runs"])))
 
+    file_path = f'./analytics-raw-data/GitHub_API-Runs-fga-eps-mds-{REPO}.json'
+
     # Salva os dados em um json file
-    with open(nome_arquivo_json, "w") as arquivo_json:
-        json.dump(data, arquivo_json)
-    
-    # _______________________________________________________________________________________________________________
+    with open(file_path, 'w') as fp:
+        fp.write(json.dumps(data))
+        fp.close()
+
+    return
+
+if __name__ == "__main__":
+    _, tag = create_release()
+
+    save_sonar_metrics(tag)
+    save_github_metrics_runs()
